e21538734b1e5a2c5bf397ba8714ac7980bef66e
iki1uc <entertrainer@web.de>
2026-06-30 21:28:46 +0200

    Add MIR object and mirShow function

--- mir-core.js
+++ mir-core.js
@@ -1,1 +1,19 @@
+export const MIR = {
+  flag: true,
+  move: "gesetzt",
+  role: "künstler",
+  pulse: 0
+};
+
+export function mirShow() {
+  MIR.pulse++;
+
+  // kleine Kunstlinie
+  const line = "-".repeat(MIR.pulse);
+
+  return {
+    text: `MIR zeigt Linie #${MIR.pulse}`,
+    art: line
+  };
+}
 
